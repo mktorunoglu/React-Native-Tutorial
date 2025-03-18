42f19e0e0e51daa168fb3011d1ae778aa8f7c37b
Mehmet Kamil Torunoğlu <mehmet.torunoglu@turksat.com.tr>
2025-03-18 18:04:59 +0300

    logo_turksat.png added

--- MyFirstApp/src/screens/LoginScreen.tsx
+++ MyFirstApp/src/screens/LoginScreen.tsx
@@ -4,12 +4,14 @@ import MyImage from '../components/images/Image';
 import MyText from '../components/texts/Text';
 import MySafeAreaView from '../components/views/SafeAreaView';
 import MyView from '../components/views/View';
+import { MyAligns } from '../enums/Aligns';
 import { MyColors } from '../enums/Colors';
+import { MyFontWeights } from '../enums/FontWeights';
 import ColorUtils from '../utils/ColorUtils';
 
 const LoginScreen: React.FC = () => {
     return (
-        <MySafeAreaView >
+        <MySafeAreaView>
             <MyView
                 isColumn={true}
                 isExpanded={true}>
@@ -24,12 +26,35 @@ const LoginScreen: React.FC = () => {
                 <MyDivider />
                 <MyView
                     isExpanded={true}
+                    alignItems={MyAligns.Center}
                     backgroundColor={ColorUtils.getColorWithOpacity(MyColors.Theme, 0.2)}>
+                    <MyView
+                        isColumn={true}
+                        isExpanded={true}>
+                        <MyView
+                            isExpanded={true}>
+                            <MyText
+                                text="4" />
+                        </MyView>
+                    </MyView>
+                    <MyView
+                        height={20} />
                     <MyText
-                        text="4" />
+                        text="Copyright © 2023, Tüm Hakları Saklıdır"
+                        fontSize={12}
+                        color={ColorUtils.getColorWithOpacity(MyColors.Black, 0.5)}
+                        fontWeight={MyFontWeights.W300} />
+                    <MyView
+                        height={10} />
+                    <MyImage
+                        path={require("../../assets/logos/logo_turksat.png")}
+                        height={20}
+                        width="100%" />
+                    <MyView
+                        height={20} />
                 </MyView>
-            </MyView >
-        </MySafeAreaView >
+            </MyView>
+        </MySafeAreaView>
     );
 };
 
